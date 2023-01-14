af68db275ecfc6852485369ba25e696bbecb9dd7
rudylacrete <rudylacrete@gmail.com>
2023-01-14 19:19:35 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is using a trained ML model built using Yolo. It can be found [here](https://huggingface.co/keremberke/yolov5m-license-plate)
+
+OpenCv is then used to blur the area detected by the model.
---
+++ lib/ImageBlur.py
@@ -0,0 +1,26 @@
+import yolov5
+import cv2
+import math
+
+class ImageBlur:
+    def __init__(self) -> None:
+        self.__model = yolov5.load('./model/best.pt')
+        self.__model.conf = 0.25  # NMS confidence threshold
+        self.__model.iou = 0.45  # NMS IoU threshold
+        self.__model.agnostic = False  # NMS class-agnostic
+        self.__model.multi_label = False  # NMS multiple labels per box
+        self.__model.max_det = 1  # maximum number of detections per image
+
+    def blurLicencePlate(self, srcPath: str, dstPath: str) -> None:
+        results = self.__model(srcPath)
+        predictions = results.pred[0]
+        boxes = predictions[:, :4] # x1, y1, x2, y2
+
+        if len(boxes) == 0:
+            raise Exception('no licence plate detected')
+        
+        box = [math.floor(e) for e in boxes[0]]
+        image = cv2.imread(srcPath)
+        plate = image[box[1]:box[3], box[0]:box[2]]
+        image[box[1]:box[3], box[0]:box[2]] = cv2.blur(plate, (55, 55))
+        cv2.imwrite(dstPath, image)
