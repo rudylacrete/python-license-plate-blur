16f652eff93b7f5c98c50e076263f29ca8ed4447
rudylacrete <rudylacrete@gmail.com>
2023-01-14 19:28:09 +0400

    small update to blur picture only if confidence is high enough

--- lib/ImageBlur.py
+++ lib/ImageBlur.py
@@ -9,14 +9,16 @@ class ImageBlur:
         self.__model.iou = 0.45  # NMS IoU threshold
         self.__model.agnostic = False  # NMS class-agnostic
         self.__model.multi_label = False  # NMS multiple labels per box
-        self.__model.max_det = 1  # maximum number of detections per image
+        self.__model.max_det = 20  # maximum number of detections per image
 
     def blurLicencePlate(self, srcPath: str, dstPath: str) -> None:
         results = self.__model(srcPath)
         predictions = results.pred[0]
+        scores = predictions[:, 4]
         boxes = predictions[:, :4] # x1, y1, x2, y2
 
-        if len(boxes) == 0:
+        # TODO make the code compatible with mutliple license plate in the same picture
+        if len(boxes) == 0 or scores[0] < 0.6:
             raise Exception('no licence plate detected')
         
         box = [math.floor(e) for e in boxes[0]]
